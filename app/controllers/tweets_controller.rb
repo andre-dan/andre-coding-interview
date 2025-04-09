67b711365638be75ed84dac4e0c077859323fd74
Andre <andrerndc@gmail.com>
2025-04-09 11:30:00 -0300

    feat: added pagination and ordered resource

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,14 @@
 class TweetsController < ApplicationController
   def index
+    page = params[:page].to_i > 0 ? params[:page].to_i : 1
+    per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 5
+
     tweets = Tweet
       .by_user(params[:user_id])
+      .order(created_at: :asc)
+      .limit(per_page)
+      .offset((page - 1) * per_page)
 
-    render json: tweets.all
+    render json: tweets
   end
 end
